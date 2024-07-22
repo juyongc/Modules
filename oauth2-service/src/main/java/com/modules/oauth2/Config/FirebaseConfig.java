9b3c29647bd8a4a5bd905129b2023b369aee491f
juyongc <chunjy2147@gmail.com>
2024-07-22 17:33:38 +0900

    [feat] firebase 기반 google oauth2 모듈 개발 - 로그아웃 및 refresh token 강제 무효화 기능 구현

--- oauth2-service/src/main/java/com/modules/oauth2/Config/FirebaseConfig.java
+++ oauth2-service/src/main/java/com/modules/oauth2/Config/FirebaseConfig.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,16 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
+
+        List<FirebaseApp> apps = FirebaseApp.getApps();
+        if (apps != null && !apps.isEmpty()) {
+            for (FirebaseApp app : apps) {
+                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    return app;
+                }
+            }
+        }
+
         FileInputStream serviceAccount = new FileInputStream("oauth2-service/src/main/resources/firebase-service-account.json");
 
         FirebaseOptions options = FirebaseOptions.builder()
--- oauth2-service/src/main/java/com/modules/oauth2/LoginController.java
+++ oauth2-service/src/main/java/com/modules/oauth2/LoginController.java
@@ -3,11 +3,16 @@ package com.modules.oauth2;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
+import com.google.firebase.auth.UserRecord;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -45,4 +50,38 @@ public class LoginController {
             return new ResponseEntity<>(new ModelAndView("unauthorized"), HttpStatus.UNAUTHORIZED);
         }
     }
+
+    /**
+     * 파이어베이스 refreshToken 갱신 시키는 기능
+     * - 기기 탈취 등으로 자동 access token 갱신을 막기 위함
+     */
+    @PostMapping("/forceout")
+    public ResponseEntity<?> forceOut(@RequestBody ForceoutRequestDto requestDto) {
+
+        String uid = requestDto.getUid();
+
+        if (uid == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            FirebaseAuth.getInstance().revokeRefreshTokens(uid);
+            UserRecord user = FirebaseAuth.getInstance().getUser(uid);
+            long revocationSecond = user.getTokensValidAfterTimestamp() / 1000;
+            log.info("Tokens revoked at: " + revocationSecond);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (FirebaseAuthException e) {
+            log.info(e.getMessage());
+            return new ResponseEntity<>(new ModelAndView("unauthorized"), HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private class ForceoutRequestDto {
+
+        private String uid;
+    }
+
 }
