752e26416c6f1da814cd9c26206de7a16f616fa6
juyongc <chunjy2147@gmail.com>
2024-09-12 11:08:46 +0900

    [refactor] access token 받기 일부 불필요한 코드 수정

--- social-oauth2-service/src/main/java/com/kakaooauth2service/KakaoLoginService.java
+++ social-oauth2-service/src/main/java/com/kakaooauth2service/KakaoLoginService.java
@@ -1,12 +1,9 @@
 package com.kakaooauth2service;
 
-import io.netty.handler.codec.http.HttpHeaderValues;
-import java.util.HashMap;
-import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
